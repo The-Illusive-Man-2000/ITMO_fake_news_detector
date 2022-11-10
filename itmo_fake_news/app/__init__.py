f07e7944f1bb8522c8228149225724127b80cd37
Dmitriy <dmitriy-345@mail.ru>
2022-11-10 07:50:58 +0300

    Add files via upload

---
+++ itmo_fake_news/app/__init__.py
@@ -0,0 +1,39 @@
+import logging.config
+import pickle
+
+import spacy
+import ru_core_news_sm
+import tensorflow_hub as hub
+import tensorflow_text
+from flask import Flask
+
+from app.config import Config
+
+
+def setup_logger(logger_name, log_file, level=logging.INFO):
+    l = logging.getLogger(logger_name)
+    formatter = logging.Formatter("[%(asctime)s] %(levelname)s in '%(module)s' at line %(lineno)d: %(message)s ")
+    fileHandler = logging.FileHandler(log_file, mode='a')
+    fileHandler.setFormatter(formatter)
+    streamHandler = logging.StreamHandler()
+    streamHandler.setFormatter(formatter)
+    rotateHandler = logging.handlers.RotatingFileHandler(log_file, maxBytes=1048576, backupCount=10)
+    l.setLevel(level)
+    l.addHandler(fileHandler)
+    l.addHandler(rotateHandler)
+    l.addHandler(streamHandler)
+    return l
+
+
+my_model_svc = pickle.load(open(Config.MODEL, 'rb'))
+
+# Загружаем модель из spacy.
+nlp = spacy.load('ru_core_news_sm')
+# Удаляем частице "не" из списка стоп-слов.
+for w in Config.deselect_stop_words:
+    nlp.vocab[w].is_stop = False
+
+# Загружаем модель USE
+use = hub.load("https://tfhub.dev/google/universal-sentence-encoder-multilingual-large/3")
+
+app = Flask(__name__)
